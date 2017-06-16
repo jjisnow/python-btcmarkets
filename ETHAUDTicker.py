d830234c907d3fbd79a3a2304622f3afc98afe71
jjisnow <jjisnow@gmail.com>
2017-06-17 07:57:04 +1000

    ETH/AUD ticker fixes - now denser and polls regularly

--- ETHAUDTicker.py
+++ ETHAUDTicker.py
@@ -8,28 +8,25 @@ from config import domain
 
 uri = "/market/ETH/AUD/tick"
 url = domain + uri
+poll_seconds = 5
 
 r = requests.get(url, verify=True)
-
 print("BTC Markets most recent ETH trade data:")
+print("{0}\n".format(r.url))
+print("bid price   : ask price   : Last trade \n")
 while True:
+    r = requests.get(url, verify=True)
     ask = str(r.json()["bestAsk"])
     bid = str(r.json()["bestBid"])
     last = str(r.json()["lastPrice"])
     tstamp = r.json()["timestamp"]
-    ltime = time.ctime(tstamp)
-    utime = time.asctime(time.gmtime(tstamp))
 
-    p = """
-        Best bid price (sell at): Best ask price (buy at) : Last trade price
-        {0} AUD                 : {1} AUD                 : {2} AUD
+    local_time=time.localtime(tstamp)
+    local=time.strftime("%Y-%m-%d %H:%M:%S",local_time)
+    utc_time=time.gmtime(tstamp)
+    utc=time.strftime("%H:%M:%S",utc_time)
 
-        Accurate at:
-        {3} (local time)
-        {4} UTC
-
-        Source:  {5}
-
-        """.format(bid, ask, last, ltime, utime, r.url)
+    p = "{0} AUD  : {1} AUD  : {2} AUD : {3} local : {4} UTC ".format(bid, ask, last, local, utc )
 
     print(p)
+    time.sleep(poll_seconds)
