a0f4e28cfc705608fb9d28aaa440550e2d9ee664
jjisnow <jjisnow@gmail.com>
2017-06-18 00:53:46 +1000

    Account Balances presented more cleanly, OrderHistory started

--- AccountBalance.py
+++ AccountBalance.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-import requests
-import time
+import base64
 import hashlib
 import hmac
-import base64
-import json
-
+import time
 from collections import OrderedDict
-from config import apikey_secret
+
+import requests
+
 from config import apikey_public
+from config import apikey_secret
 from config import domain
 
 # Define Global Vars
@@ -18,6 +18,7 @@ url = domain + uri
 askey = apikey_secret.encode("utf-8")
 pkey = apikey_public.encode("utf-8")
 skey = base64.standard_b64decode(askey)
+
 
 def build_headers(URL, PUBKEY, PRIVKEY):
     """Build timestamp, format and encode everything,  and construct string to 
@@ -31,40 +32,40 @@ def build_headers(URL, PUBKEY, PRIVKEY):
     tstamp = time.time()
     ctstamp = int(tstamp * 1000)  # or int(tstamp * 1000) or round(tstamp * 1000)
     sctstamp = str(ctstamp)
-    
+
     # Optional timestamping method, not recommended as ticker results may
     # be cached beyond the authentication window.
     #
-    #urt = "/market/BTC/AUD/tick"
-    #turl = domain + urt
-    #rt = requests.get(turl, verify=True)
-    #tstamp = r.json()["timestamp"]
-    #ctstamp = tstamp * 1000
+    # urt = "/market/BTC/AUD/tick"
+    # turl = domain + urt
+    # rt = requests.get(turl, verify=True)
+    # tstamp = r.json()["timestamp"]
+    # ctstamp = tstamp * 1000
 
-   
+
     # Build and sign to construct body
     sbody = uri + "\n" + sctstamp + "\n"
     rbody = sbody.encode("utf-8")
     rsig = hmac.new(skey, rbody, hashlib.sha512)
     bsig = base64.standard_b64encode(rsig.digest()).decode("utf-8")
-    
+
     # Construct header list of key value pairs
     headers_list = OrderedDict([("Accept", "application/json"),
-                     ("Accept-Charset", "UTF-8"),
-                     ("Content-Type", "application/json"),
-                     ("apikey", pkey),
-                     ("timestamp", sctstamp),
-                     ("signature", bsig)])
+                                ("Accept-Charset", "UTF-8"),
+                                ("Content-Type", "application/json"),
+                                ("apikey", pkey),
+                                ("timestamp", sctstamp),
+                                ("signature", bsig)])
     # Load list into dictionary
     headers = dict(headers_list)
-    
+
     return headers
 
 
 def main():
     """ Build the request body by invoking header function with config
     params specified as global variables at top and return the balances for 
-    AUD, BTC and LTC. 
+    AUD, ETH, BTC and LTC.
     
     TODO: Add in functionality to pass options for the CLI.
     """
@@ -72,13 +73,18 @@ def main():
     res = build_headers(url, pkey, skey)
     r = requests.get(url, headers=res, verify=True)
 
+    # returns balances*1e8 units
     audbal = r.json()[0]
     btcbal = r.json()[1]
     ltcbal = r.json()[2]
+    ethbal = r.json()[3]
 
-    print(audbal)
-    print(btcbal)
-    print(ltcbal)
+    print(r.json())
+    print("${:.2f} AUD Balance, ${:.2f} AUD pending".format(audbal["balance"] / 1e8, audbal["pendingFunds"] / 1e8))
+    print("{:.6f} ETH Balance, {:.6f} ETH pending".format(ethbal["balance"] / 1e8, ethbal["pendingFunds"] / 1e8))
+    print("{:.6f} BTC Balance, {:.6f} BTC pending".format(btcbal["balance"] / 1e8, btcbal["pendingFunds"] / 1e8))
+    print("{:.6f} LTC Balance, {:.6f} LTC pending".format(ltcbal["balance"] / 1e8, ltcbal["pendingFunds"] / 1e8))
+
 
 if __name__ == "__main__":
     main()
---
+++ OrderHistory.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import base64
+import hashlib
+import hmac
+import time
+from collections import OrderedDict
+import json
+import requests
+
+from config import apikey_public
+from config import apikey_secret
+from config import domain
+
+# Define Global Vars
+uri = "/order/history"
+url = domain + uri
+api_secret_key = apikey_secret.encode("utf-8")
+# pub_key = apikey_public.encode("utf-8")
+pub_key = apikey_public
+std_secret_key = base64.standard_b64decode(api_secret_key)
+
+
+def build_headers(URL, PUBKEY, PRIVKEY):
+    """Build timestamp, format and encode everything,  and construct string to 
+    sign with api key. Use HmacSHA512 algorithm in order to sign.
+    
+    Lastly build the headers to send... In order to ensure the correct order 
+    of key value pairs in the JSON payload, build an ordered dictionary out
+    of a list of tuples.
+    """
+
+    # Build timestamp
+    tstamp = time.time()
+    ctstamp = int(tstamp * 1000)  # or int(tstamp * 1000) or round(tstamp * 1000)
+    str_ctstamp = str(ctstamp)
+
+    # Build Request component
+    body = OrderedDict([("currency", "AUD"), ("instrument", "ETH"), ("limit", 10), ("since", 429357237)])
+
+    # Build and sign to construct body
+    sbody = uri + "\n" + str_ctstamp + "\n" + json.dumps(body) + "\n"
+
+    # # Fudge an ordered dictionary string
+    # for _ in range(len(body)):
+    #     s = body.popitem(last=False)
+    #     sbody += '\"' + str(s[0]) + '\":'
+    #     if type(s[1]) == type(''):
+    #         sbody += '\"' + str(s[1]) + '\"'
+    #     else:
+    #         sbody += str(s[1])
+    #     sbody += ","
+    # sbody = sbody[:-1] + '}'
+    print(repr(sbody))
+
+    # dictionary string
+    rbody = sbody.encode("utf-8")
+    rsig = hmac.new(std_secret_key, rbody, hashlib.sha512)
+    bsig = base64.standard_b64encode(rsig.digest()).decode("utf-8")
+
+    print(pub_key)
+    # Construct header list of key value pairs
+    headers_list = OrderedDict([("Accept", "application/json"),
+                                ("Accept-Charset", "UTF-8"),
+                                ("Content-Type", "application/json"),
+                                ("apikey", pub_key),
+                                ("timestamp", str_ctstamp),
+                                ("signature", bsig)])
+
+    # Load list into dictionary
+    # headers = dict(headers_list)
+
+    # http://docs.python-requests.org/en/master/user/advanced/#header-ordering
+    # maybe returning the ordered list to requests may provide headers with ordering
+    return json.dumps(headers_list)
+
+
+def main():
+    """ Build the request body by invoking header function with config
+    params specified as global variables at top
+    
+    TODO: Add in functionality to pass options for the CLI.
+    """
+
+    res = build_headers(url, pub_key, std_secret_key)
+    print(res)
+    r = requests.post(url=url, json=res)
+
+    # try:
+    print(r)
+    print(r.headers)
+    # print(r.json())
+
+if __name__ == "__main__":
+    main()
